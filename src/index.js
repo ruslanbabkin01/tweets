2e1b7e9dd89efbda4f64e1ca816890288ab23d71
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-04-29 22:59:56 +0300

    add 404 html

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyles, theme } from '../src/styles/index';
 import { Global, ThemeProvider } from '@emotion/react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <Global styles={GlobalStyles} />
-    <HashRouter>
+    <BrowserRouter basename="/tweets">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ThemeProvider>
 );
