e7f97e89437e5758227fe523b0d3fed5c5f17b3e
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-04-30 22:57:48 +0300

    remove gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')  as HTMLElement
 root.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <BrowserRouter basename="/tweets">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>
